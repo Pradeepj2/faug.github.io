fe507a6654ed4b130e1da196f901c8d616567afc
Pradeepj2 <70266771+Pradeepj2@users.noreply.github.com>
2020-09-06 22:20:45 +0530

    faug upcoming game

---
+++ faug.js
@@ -0,0 +1,6 @@
+
+// var img = document.getElementsByClassName('main-contianer');
+// var target = document.getElementsByClassName("submitbtn");
+//  target.addEventListener("focus",function(){
+//     img.style.filter=blur(2px);  
+// });
